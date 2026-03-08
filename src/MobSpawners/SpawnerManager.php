a8ee3a02997c5986d45ed8fe744086afbe36fb33
ZyberPE <zyberpe@gmail.com>
2026-03-08 15:31:12 -0400

    Create SpawnerManager.php

---
+++ src/MobSpawners/SpawnerManager.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace MobSpawners;
+
+use pocketmine\block\VanillaBlocks;
+use pocketmine\item\Item;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\world\Position;
+
+class SpawnerManager{
+
+    private Main $plugin;
+
+    public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+    }
+
+    public function createSpawner(string $type,int $level): Item{
+
+        $item = VanillaBlocks::MOB_SPAWNER()->asItem();
+
+        $nbt = $item->getNamedTag();
+        $nbt->setString("spawner_type",$type);
+        $nbt->setInt("spawner_level",$level);
+
+        $item->setNamedTag($nbt);
+
+        $config = $this->plugin->getConfig();
+
+        $name = $config->get("spawners")[$type]["name"];
+
+        $item->setCustomName("§r§e".$name);
+
+        $item->setLore([
+            "§7Level: §e".$level
+        ]);
+
+        return $item;
+    }
+
+    public function getSpawnerLevel(Item $item): int{
+        return $item->getNamedTag()->getInt("spawner_level",1);
+    }
+
+    public function getSpawnerType(Item $item): string{
+        return $item->getNamedTag()->getString("spawner_type","");
+    }
+}
